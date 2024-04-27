c9d478a0a076cca26e31243c13cc18daad84dc71
bhupey <bhupendra.neupane08@gmail.com>
2024-04-28 00:54:23 +0545

    progress

--- Frontend/guest_guru_frontend/src/components/Pages/Home.js
+++ Frontend/guest_guru_frontend/src/components/Pages/Home.js
@@ -192,7 +192,7 @@ function Home() {
               lessons. 'Never again' has turned into 'again and again and
               again.' So today, ladies and gentlemen, take Hotel Lunar as a
               wake-up call and a message to be our messenger that people are the
-              ones who can change what they want to change.
+              ones who can change what they want to change. Ladies and gentlemen, history keeps repeating itself but doesn't teach us any lessons. 'Never again' has turned into 'again and again and again.' So today, ladies and gentlemen, take Hotel Lunar as a wake-up call and a message to be our messenger that people are the ones who can change what they want to change.
             </p>
             <div>
               <img
